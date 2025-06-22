1b3da7630a3d1b815d7684eaf377858fb9832a57
mborhade <mborhade@users.noreply.github.com>
2025-06-22 09:04:01 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 # Variables
 IMAGE_NAME="my-python-app"
 IMAGE_TAG="latest"
-REGISTRY_URL="atuljkamble"
+REGISTRY_URL="mangeshborhade"
 
 echo "Logging into Docker..."
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
